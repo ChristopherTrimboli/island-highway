19adf388489b8b823883ed954a0d8ffded95455c
Chris Eddy <ceddy.zen@gmail.com>
2018-09-03 18:40:07 -0700

    Add socials, change title, CSS fix text overflow

--- src/components/getInvolved.js
+++ src/components/getInvolved.js
@@ -5,6 +5,7 @@ class GetInvolved extends Component {
   render() {
     return (
       <div className="GetInvolved" id='Contact'>
+
         <div className='row pt-5 pb-3'>
           <div className='col-12'>
             <i className="far fa-comments fa-4x pt-3"/>
@@ -13,13 +14,32 @@ class GetInvolved extends Component {
           <div className='col-12'>
             <h4 className='pt-3'>Send us your comments!</h4>
           </div>
-          <div className='col-12 pt-5'>
+        </div>
+
+        <div className='row'>
+          <div className='col-lg-4 col-md-4 col-sm-12 col-xs-12 pt-5'>
+            <a href='https://twitter.com/NewIslandHwy'>
+              <i className="fab fa-twitter fa-2x p-3"/>
+              <h5 className='pb-5'>Follow Us on Twitter</h5>
+            </a>
+          </div>
+
+          <div className='col-lg-4 col-md-4 col-sm-12 col-xs-12 pt-5'>
             <a href='mailto:newsouthvancouverislandhwy@shaw.ca'>
               <i className="far fa-envelope fa-2x p-3"/>
               <h5 className='pb-5'>newsouthvancouverislandhwy@shaw.ca</h5>
             </a>
           </div>
+
+          <div className='col-lg-4 col-md-4 col-sm-12 col-xs-12 pt-5'>
+            <a href='https://www.facebook.com/newislandhighway'>
+              <i className="fab fa-facebook-square fa-2x p-3"/>
+              <h5 className='pb-5'>Like our page on Facebook</h5>
+            </a>
+          </div>
         </div>
+
+
       </div>
     );
   }
--- src/components/titleSection.js
+++ src/components/titleSection.js
@@ -25,7 +25,7 @@ class TitleSection extends Component {
           <div className='jumboInner'>
             <h1 className="display-4">An Alternate Vancouver Island Highway</h1>
             <p className="lead pt-3">The Malahat highway has proved dangerous and expensive...</p>
-          <p className='lead'>But still, our government's best idea is to continue expanding its madness.</p>
+          <p className='lead'>But still, our government's best idea is to continue expanding the Malahat.</p>
             <hr className="my-4"/>
             <p>This website covers the 10, 20 and 50 year plans for a safer, easier and more functional highway system.</p>
             <p>Let your opinion be heard! Leave a simple 'yes' or 'no' vote.</p>
